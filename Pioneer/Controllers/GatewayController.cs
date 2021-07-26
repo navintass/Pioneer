813b1bb5e631dbfa379a2bb1dbb96f91e4158ce5
Apiwat Loket <apiwat021@hotmail.com>
2021-07-27 00:34:18 +0700

    fist commit

---
+++ Pioneer/Controllers/GatewayController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Pioneer.Controllers
+{
+    public class GatewayController : Controller
+    {
+        // GET: Gateway
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+    }
+}
