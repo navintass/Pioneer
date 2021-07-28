53352b395962495c336c6790803d964e4f209257
Apiwat Loket <apiwat021@hotmail.com>
2021-07-28 23:52:40 +0700

    add class

---
+++ Pioneer/Class/CLASS_MODEL/MODEL_APP.cs
@@ -0,0 +1,22 @@
+﻿using Pioneer.Linq;
+using Pioneer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Pioneer.Class.CLASS_MODEL
+{
+    public class MODEL_APP
+    {
+        public string FUNC_CODE { get; set; }
+        public Models.Register register { get; set; }
+        public BANK_TRANSFER BANK_TRANSFER { get; set; }
+        public BANK_TRANSFER_DETAIL BANK_TRANSFER_DETAIL { get; set; }
+        public ACCOUNT_DELIVERY_SETTING ACCOUNT_DELIVERY_SETTING { get; set; }
+        public MEMBER MEMBER { get; set; }
+        public PRODUCT PRODUCT { get; set; }
+        public PRODUCER PRODUCER { get; set; }
+
+    }
+}
--- Pioneer/Controllers/GatewayController.cs
+++ Pioneer/Controllers/GatewayController.cs
@@ -1,6 +1,10 @@
-﻿using System;
+﻿using Microsoft.VisualBasic;
+using Microsoft.VisualBasic.CompilerServices;
+using Pioneer.Class.CLASS_MODEL;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.RegularExpressions;
 using System.Web;
 using System.Web.Mvc;
 
@@ -14,6 +18,10 @@ namespace Pioneer.Controllers
             return View();
         }
 
-
+        public JsonResult SET_FULL_MODEL()
+        {
+            var CLS_MODEL = new MODEL_APP();
+            return Json(CLS_MODEL, JsonRequestBehavior.AllowGet);
+        }
     }
 }
